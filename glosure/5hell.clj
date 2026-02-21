ddcf7db7738bbd220ada98ab0b14b605f582a728
Sophie <176602700+shippingfandom@users.noreply.github.com>
2026-02-21 19:56:54 +0500

    The first ever commit!

---
+++ glosure/5hell.clj
@@ -0,0 +1,164 @@
+;; 
+;; Standard Glosure Library, modified
+;; 
+
+;; Define undefined variable or use value of the defined one
+(defmacro defdefault (name value) ()
+    (def name
+        (if (hasIndex (context) (quote name))
+            name
+            value)))
+
+;; Define named lambda
+(defmacro defun (name arguments body) ()
+    (def name (lambda arguments body)))
+
+;; Define named glosure
+(defmacro defunction (name arguments body) ()
+    (def name (glosure arguments body)))
+
+;; While loop
+(defmacro while (condition body) ()
+    (if condition (loop body condition)))
+
+;; Do while loop
+(defmacro do-while (condition body) ()
+    (loop body condition))
+
+;; For loop
+(defmacro for (initializer condition iterator body) () (begin
+    initializer
+    (if condition (loop body iterator condition))))
+
+;; Foreach loop
+(defmacro foreach (key value collection body) (!keys) (begin
+    (defdefault !keys (array))
+    (push !keys (indexes collection))
+    (if (at !keys (to_int '-1'))
+        (loop
+            (def key (pull (at !keys (to_int '-1'))))
+            (def value (at collection key))
+            body
+            (at !keys (to_int '-1'))))
+    (pop !keys)))
+
+;; Rebind keyword
+(defmacro defalias (name keyword) ()
+    (defmacro name () () keyword))
+
+;; Swap two variables
+(defmacro swap (a b) (!temp) (begin
+    (def !temp a)
+    (def a b)
+    (def b !temp)))
+
+;; Preincrement
+(defmacro ++ (var) ()
+    (def var (+ var 1)))
+
+;; Postincrement
+(defmacro var++ (var) (!temp) (begin
+    (def !temp var)
+    (def var (+ var 1))
+    !temp))
+
+;; Predecrement
+(defmacro -- (var) ()
+    (def var (- var 1)))
+
+;; Postdecrement
+(defmacro var-- (var) (!temp) (begin
+    (def !temp var)
+    (def var (- var 1))
+    !temp))
+
+;; Useless gensym
+(defun gensym ()
+    (exec '(defmacro _ () (sym) (quote sym))(_)')) ;; Unquote is needed to make it any usefull
+
+
+
+
+;; 
+;; 5hell Glosure Library
+;;
+
+;; Get COB variable
+(defmacro @ (var-name) ()
+    (if (hasIndex (get_custom_object) (quote var-name))
+        (at (get_custom_object) (quote var-name))))
+
+;; Set COB variable
+(defmacro @= (var-name var-value) ()
+    (set (get_custom_object) (quote var-name) var-value))
+
+;; Get globals variable
+(defmacro : (var-name) ()
+    (if (hasIndex globals (quote var-name))
+        (at globals (quote var-name))))
+
+;; Set globals variable
+(defmacro := (var-name var-value) ()
+    (set globals (quote var-name) var-value))
+
+;; Get globals.command command
+(defmacro |> (cmd-name) ()
+    (if (hasIndex (: command) (quote cmd-name))
+        (at (: command) (quote cmd-name))))
+
+;; Sets globals.command.cmd-name (doesn't work because of Glosure bug)
+(defmacro |= (cmd-name cmd-args cmd-body) () (begin
+    (defun cmd-name cmd-args cmd-body)
+    ((lambda () (begin
+        (def commands (: command))
+        (set commands (quote cmd-name) cmd-name)
+        (:= command commands))))))
+
+;; Get miniscript's object method
+(defmacro _ (object method) ()
+    (at object (quote method)))
+
+;; Break through globals.SILENT and print()
+(defun gl-break-silence (message) (begin
+    (def silent-state (: SILENT))
+    (if (!= silent-state 0)
+        (:= SILENT 0))
+    (print message)
+    (:= SILENT silent-state)
+    message))
+
+;; Greps into every step from previous step and returns a file if successfull
+(defun gl-grep-into (steps) (begin
+    (def from ((|> file) '-r' '/'))
+    (foreach _ step steps
+        (if (== (typeof from) 'file')
+            (def from ((|> grep) '-f' step from))))
+    (if (== (typeof from) 'file')
+        from)))
+
+;; Concats file paths
+(defun gl-pathcat (paths) (begin
+    (def path (pull paths))
+    (foreach _ part paths
+        (def path (join (array path part) '/')))
+    (if (== '//' (slice path 0 2))
+        (slice path 2 (len path)))
+        path))
+
+;; Generates random string of specified length
+(defun gl-random-string (length) (begin
+    (def abc 'acdfeghhijklmnopqrstuvwxyz')
+    (def abc (+ abc (upper abc)))
+    (def return-string '')
+    (for (def i 0) (< i length) (++ i) (begin
+        (def characters (values abc))
+        (shuffle characters)
+        (def character (pull characters))
+        (def return-string (+ return-string character))))
+    return-string))
+
+
+
+
+;; Init
+(gl-break-silence '[5GL] 5hell Glosure library is successfully loaded! \\(^.^)/')
---
+++ glosure/marinette.clj
@@ -0,0 +1,79 @@
+;; 
+;; Marinette library
+;;
+
+;; Load custom theme
+(defun mari-load-theme (theme) (begin
+    (def grep (gl-grep-into (array '^rkit$' '^marinette$' theme)))
+    (if grep (begin
+        (def content (trim ((_ grep get_content) grep)))
+        (def lines (split content (char 10)))
+        (if content
+            (foreach _ line lines (begin
+                (def parts (split line ' '))
+                (def color-name (at parts 0))
+                (def color-value (at parts 2))
+                (set globals color-name ((_ color-value replace) color-value (char 34) ''))
+                (:= colorDefault ((_ (: colorGreen) replace) (: colorGreen) '<b>' ''))))
+            (gl-break-silence (join (array '[Mari] Could not load ' theme ' theme!') ''))))
+        (gl-break-silence (join (array '[Mari] Could not load ' theme ' theme!') '')))))
+
+;; Populate rkit/marinette w/ a directory
+(defun mari-mkdir (dirname) (begin
+    (def marinette (gl-grep-into (array '^rkit$' '^marinette$')))
+    (if marinette
+        ((|> mkdir) (gl-pathcat (array ((_ marinette path) marinette) dirname))))))
+
+;; Return args from macros @pipe
+(defun mari-args () (begin
+    (def args (@ PIPE))
+    (@= PIPE '')
+    (if (== (typeof args) 'string')
+        (split args ' ')
+        (array ''))))
+
+;; Return arg, parsed, from macros @pipe[idx]
+(defun # (args idx fallback) (begin
+    (if (hasIndex args idx)
+        (if (at args idx)
+            (at args idx)
+            fallback)
+        fallback)))
+
+;; Sets 5hell macro
+(defmacro >> (macroname args macrobody) () (begin
+    (defun macroname () (begin
+        (def args (mari-args))
+        macrobody))
+    ((|> macro) '-s' (quote macroname) (join (array 'glosure -e \" (:= SILENT 2) \" | | cob set PIPE \@pipe | | glosure -e \" (:= SILENT 0) \" | | glosure -e (' (quote macroname) ')') ''))))
+
+;; Sorts 5hell macros
+(defun mari-sort-macros () (begin
+    (def macros (@ macros))
+    (def keys (indexes macros))
+    (def sorted (dict))
+    (sort keys)
+    (foreach _ key keys (begin
+        (set sorted key (at macros key))))
+    (@= macros sorted)))
+
+;; Creates hidden directory with random name
+(defun mari-mkdir-hidden-infil (computer) (begin
+    (def root ((_ computer File) computer '/'))
+    (def folders ((_ root get_folders) root))
+    (def return null)
+    (while folders (begin
+        (def folder (pull folders))
+        (def folder-name (+ '.' (gl-random-string 5)))
+        (if (== 1 ((_ computer create_folder) computer ((_ folder path) folder) folder-name)) (begin
+            (def folders (array))
+            (def return ((_ computer File) computer (gl-pathcat (array ((_ folder path) folder) folder-name)))))
+        (begin
+            (def folders (+ folders ((_ folder get_folders) folder)))))))
+    return))
+
+
+
+
+;; Init
+(gl-break-silence '[Mari] Marinette is successfully loaded! \\(^.^)/')
